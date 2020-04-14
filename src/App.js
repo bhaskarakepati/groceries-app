a631ce13e63c9bf82a93f3d2b8ebbade00cd5a89
anil-akepati1202 <anil.akepati1202@gmail.com>
2020-04-14 12:14:10 +0100

    adding navbar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import HomePage from "./components/pages/homepage/homepage.component";
 import SignIn from "./components/pages/sign-in/sign-in.component";
 import SignUp from "./components/pages/sign-up/sign-up.component";
 import Header from "./components/header/header.component";
-
+import NavigationBar from "./components/header/navigationBar";
+import Layout from "./components/Layout";
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/signup" component={SignUp} />
-        </Switch>
-      </div>
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+          </Switch>
+        </Layout>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/header/navigationBar.jsx
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react'
+import {Nav, Navbar,NavDropdown, Form, FormControl, Button} from "react-bootstrap";
+
+export class navigationBar extends Component {
+    render() {
+        return (
+            <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/signin">SignIn</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        )
+    }
+}
+
+export default navigationBar
