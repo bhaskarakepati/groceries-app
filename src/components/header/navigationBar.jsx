dfd2895fbc2320a99a00a8419b04f37a5feb84f0
bhaskarakepati <bhaskarakepati@gmail.com>
2020-04-16 01:06:01 +0100

    Designing plp  page

--- src/components/header/navigationBar.jsx
+++ src/components/header/navigationBar.jsx
@@ -1,30 +1,70 @@
 
-import React, { Component } from 'react'
-import {Nav, Navbar,NavDropdown, Form, FormControl, Button} from "react-bootstrap";
-
+import React, { Component, Fragment } from 'react'
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button, Container, Row, Col, Image, Dropdown } from "react-bootstrap";
+import './navigationBar.scss';
+import logo from '../../assets/logo.png'
 export class navigationBar extends Component {
-    render() {
-        return (
-            <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  render() {
+    return (
+      <Fragment>
+        <Container >
+          <Row md={4} lg={4} className="header_items m-2">
+            <Col>
+              <a href="/" class="header__logo"></a>
+              <Image src={logo} rounded width className="img-fluid" />
+            </Col>
+            <Col>
+              <span><b>Call us 01274 317178</b> <br />Mon - Sun 9:00 - 18:00</span>
+
+            </Col>
+            <Col className="profile">
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Sign In <br /> Your Account
+              </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Sign In</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Sign Up</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+          </Row>
+        </Container>
+        <Container >
+          <Navbar expand="lg" className="border-top border-bottom font">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
+              <Nav className="m-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/orderNow">Order Now</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <Nav.Link href="/orderNow">Vegies</Nav.Link>
+                <NavDropdown title="Grains" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="/signin">SignIn</Nav.Link>
+                <Nav.Link href="/orderNow">Spices</Nav.Link>
+                <Nav.Link href="/">Healthy Options</Nav.Link>
+                <NavDropdown title="Sweets & Snacks" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/orderNow">Spices</Nav.Link>
+                <Nav.Link href="/">Healthy Options</Nav.Link>
+                
+                <Nav.Link href="/signin">Froozen Foods</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
-        )
-    }
+        </Container >
+      </Fragment>
+    )
+  }
 }
 
 export default navigationBar
--- src/components/pages/plp/plp.component.jsx
+++ src/components/pages/plp/plp.component.jsx
@@ -1,26 +1,50 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import Product from './product';
+import { Container, Row, Col } from "react-bootstrap";
+
+import Product from '../product/product';
 import './plp.styles.scss';
 export default class plp extends Component {
-    state = {
-        persons: []
-      }
-  
+    constructor(props) {
+        super(props);
+        this.state = {
+            persons: []
+        }
+    }
+
     componentDidMount() {
         axios.get(`http://localhost:8000/products`)
-          .then(res => {
-            const persons = res.data;
-            console.log(persons);
-            this.setState({ persons });
-          })
-      }
+            .then(res => {
+                const persons = res.data;
+                console.log(persons);
+                this.setState({ persons });
+            })
+    }
     render() {
         return (
             <React.Fragment>
-                <div className="row products_list">
-                { this.state.persons.map(person => <Product product={person} />)}
-                </div>
+                <Row className="mt-3">
+                    <Col lg={3} >
+                       <Row>
+                           <ul className="menu_Filter">
+                               <li>Vegies</li>
+                               <li>Grains</li>
+                               <li>Spices</li>
+                               <li>Healthy Options</li>
+                               <li>Vegies</li>
+                               <li>Grains</li>
+                               <li>Spices</li>
+                               <li>Healthy Options</li>
+                           </ul>
+                       </Row>
+                    </Col>
+                    <Col md={12} lg={9} className="">
+                        <div className="row products_list border-bg">
+                            {this.state.persons.map(person => <Product product={person} />)}
+                        </div>
+                    </Col>
+                </Row>
+
             </React.Fragment>
         )
     }
